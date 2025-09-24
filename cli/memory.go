8bcf247471e73f60d2946456f19f529dcf36f7f6
Matt Keranen <mkeranen@shccares.com>
2025-09-24 22:39:09 +0000

    Fix nil pointer deref

--- cli/memory.go
+++ cli/memory.go
@@ -14,19 +14,19 @@ func (a *App) MemoryLoop() {
 			} else {
 				a.ui.header.GetCell(0, 4).Text = fmt.Sprintf("keys: %d", m.KeysCount)
 				a.ui.header.GetCell(0, 5).Text = fmt.Sprintf("mem: %s", DisplayUnit(float64(m.PeakAllocated)))
-			}
-			kv, err := a.redis.Info()
-			if err != nil {
-				a.log.Printf("Info Memory Error : %s", err.Error())
-			} else {
-				a.ui.app.QueueUpdate(func() {
-					a.ui.memories.SetTitle(fmt.Sprintf("Memory [ %s ]", kv["maxmemory_policy"]))
-					for i, line := range m.Table() {
-						for j, col := range line {
-							a.ui.memories.GetCell(i, j).Text = col
+				kv, err := a.redis.Info()
+				if err != nil {
+					a.log.Printf("Info Memory Error : %s", err.Error())
+				} else {
+					a.ui.app.QueueUpdate(func() {
+						a.ui.memories.SetTitle(fmt.Sprintf("Memory [ %s ]", kv["maxmemory_policy"]))
+						for i, line := range m.Table() {
+							for j, col := range line {
+								a.ui.memories.GetCell(i, j).Text = col
+							}
 						}
-					}
-				})
+					})
+				}
 			}
 
 			time.Sleep(5 * time.Second)
